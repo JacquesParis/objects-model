8e24609f9aaeba54a4892eb996a9cdde4c49244c
Jacques Lebourgeois <jacques@ochoeurdunet.org>
2020-12-05 01:00:07 +0100

    server entity definition

--- src/model/i-object-type.ts
+++ src/model/i-object-type.ts
@@ -4,6 +4,7 @@ import {IRestEntity} from './i-rest-entity';
 export interface IObjectType extends IRestEntity {
   name: string;
   contentType: string;
+  inheritedTypesIds: string[];
   definition: any;
   contentDefinition?: any;
   objectSubTypes: IObjectSubType[];
--- src/model/i-rest-entity.ts
+++ src/model/i-rest-entity.ts
@@ -1,5 +1,22 @@
 import {IDataEntity} from './i-data-entity';
+import {IJsonSchema} from './i-json-shema';
 
+export interface IAccessRightCRUD {
+  delete: boolean;
+  create: boolean;
+  read: boolean;
+  update: boolean;
+}
+export interface IAclCtx {
+  rights: IAccessRightCRUD;
+}
 export interface IRestEntity extends IDataEntity {
   uri?: string;
+  entityCtx?: {
+    entityDefinition?: IJsonSchema;
+    aclCtx?: IAclCtx;
+    loaded?: boolean;
+    actions?: {creations?: {[id: string]: IJsonSchema}; reads?: string[]};
+  };
+  entityDefinition?: IJsonSchema;
 }
